3efd785c46e3d38084db8bc9fd746381d54eed20
wampirusya <irynasurovtseva@gmail.com>
2023-11-29 22:51:02 +0200

    update

---
+++ js/mobile-menu.js
@@ -0,0 +1,11 @@
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuBtnOpen = document.querySelector('.menu-btn-open');
+const menuBtnClose = document.querySelector('.menu-btn-close');
+
+const toggleMenu = () => mobileMenu.classList.toggle('is-open');
+const disableScroll = () =>
+  document.body.classList.toggle('is-scroll-disabled');
+
+menuBtnOpen.addEventListener('click', toggleMenu);
+menuBtnClose.addEventListener('click', toggleMenu);
+
